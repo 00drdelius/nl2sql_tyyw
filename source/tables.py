409fd6bda508f3f1236095f9ad1c2cb171ae9ddf
Delius <1092104983@qq.com>
2026-05-29 00:11:34 +0800

    update: example for in-context learning

--- source/tables.py
+++ source/tables.py
@@ -7,22 +7,28 @@ import rich
 
 attendance_tables_file = Path(__file__).parent / "考勤数据表定义.md"
 bpm_tables_file = Path(__file__).parent / "工单流程数据定义.md"
+example_tables_file = Path(__file__).parent / "example.md"
 
 table_name_matcher = re.compile(r"(?<=# \d\. )\w+(?= \[)")
 
 with attendance_tables_file.open('r') as f:
-    attn_raw = f.read()
+    ATTDN_RAW = f.read()
 
 with bpm_tables_file.open('r') as f:
-    bpm_raw = f.read()
+    BPM_RAW = f.read()
 
+with example_tables_file.open('r') as f:
+    EXAMPLE_RAW = f.read()
 
-ATT_TABLENAMES=table_name_matcher.findall(attn_raw)
-BPM_TABLENAMES=table_name_matcher.findall(bpm_raw)
+ATTDN_TABLENAMES=table_name_matcher.findall(ATTDN_RAW)
+BPM_TABLENAMES=table_name_matcher.findall(BPM_RAW)
+EXAMPLE_TABLENAMES=table_name_matcher.findall(EXAMPLE_RAW)
 # rich.print(ATT_TABLENAMES)
 # rich.print(BPM_TABLENAMES)
 
 splitter="<br>"
-ATTN_TABLE_SCHEMAS=[i.strip() for i in attn_raw.split(splitter)]
-BPM_TABLE_SCHEMAS=[i.strip() for i in bpm_raw.split(splitter)]
+ATTDN_TABLE_SCHEMAS=[i.strip() for i in ATTDN_RAW.split(splitter)]
+BPM_TABLE_SCHEMAS=[i.strip() for i in BPM_RAW.split(splitter)]
+EXAMPLE_TABLE_SCHEMAS=[i.strip() for i in EXAMPLE_RAW.split(splitter)]
+
 
